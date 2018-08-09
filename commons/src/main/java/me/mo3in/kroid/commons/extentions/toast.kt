f00678bf880bf507baedadcc3da4f66a0525c755
Mo3in <moein.hente@gmail.com>
2018-08-09 13:23:23 +0430

    add toast, gson extensions

---
+++ commons/src/main/java/me/mo3in/kroid/commons/extentions/toast.kt
@@ -0,0 +1,20 @@
+package me.mo3in.kroid.commons.extentions
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.toast(resId: Int) {
+    Toast.makeText(this, resId, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Context.longToast(resId: Int) {
+    Toast.makeText(this, resId, Toast.LENGTH_LONG).show()
+}
