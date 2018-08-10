93c9e2fe69258173926d66bd2b3a5fb1c2d6f076
Mo3in <moein.hente@gmail.com>
2018-08-10 12:15:18 +0430

    dimensions

---
+++ libs/commons/src/main/java/me/mo3in/kroid/commons/extentions/dimensions.kt
@@ -0,0 +1,40 @@
+package me.mo3in.kroid.commons.extentions
+
+import android.content.Context
+import android.util.DisplayMetrics
+import android.view.View
+
+const val LDPI: Int = DisplayMetrics.DENSITY_LOW
+const val MDPI: Int = DisplayMetrics.DENSITY_MEDIUM
+const val HDPI: Int = DisplayMetrics.DENSITY_HIGH
+
+const val TVDPI: Int = DisplayMetrics.DENSITY_TV
+const val XHDPI: Int = DisplayMetrics.DENSITY_XHIGH
+const val XXHDPI: Int = DisplayMetrics.DENSITY_XXHIGH
+const val XXXHDPI: Int = DisplayMetrics.DENSITY_XXXHIGH
+
+const val MAXDPI: Int = 0xfffe
+
+
+//returns dip(dp) dimension value in pixels
+fun Context.dip(value: Int): Int = (value * resources.displayMetrics.density).toInt()
+fun Context.dip(value: Float): Int = (value * resources.displayMetrics.density).toInt()
+
+//return sp dimension value in pixels
+fun Context.sp(value: Int): Int = (value * resources.displayMetrics.scaledDensity).toInt()
+fun Context.sp(value: Float): Int = (value * resources.displayMetrics.scaledDensity).toInt()
+
+//converts px value into dip or sp
+fun Context.px2dip(px: Int): Float = px.toFloat() / resources.displayMetrics.density
+fun Context.px2sp(px: Int): Float = px.toFloat() / resources.displayMetrics.scaledDensity
+
+fun Context.dimen(resource: Int): Int = resources.getDimensionPixelSize(resource)
+
+//the same for the views
+fun View.dip(value: Int): Int = context.dip(value)
+fun View.dip(value: Float): Int = context.dip(value)
+fun View.sp(value: Int): Int = context.sp(value)
+fun View.sp(value: Float): Int = context.sp(value)
+fun View.px2dip(px: Int): Float = context.px2dip(px)
+fun View.px2sp(px: Int): Float = context.px2sp(px)
+fun View.dimen( resource: Int): Int = context.dimen(resource)
