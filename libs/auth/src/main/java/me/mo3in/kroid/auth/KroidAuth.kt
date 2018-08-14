3ebbc285bddeb0b5a809c045c1fd0f0f6b7d7eaf
Mo3in <moein.hente@gmail.com>
2018-08-14 19:34:43 +0430

    auth init

---
+++ libs/auth/src/main/java/me/mo3in/kroid/auth/KroidAuth.kt
@@ -0,0 +1,40 @@
+package me.mo3in.kroid.auth
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import me.mo3in.kroid.auth.models.BaseUserRegisterModel
+
+object KroidAuth {
+
+    fun isSignIn(): Boolean {
+        return false
+    }
+
+    fun <T> getUserInfo(): Observable<T> {
+        return Observable.just(T)
+    }
+
+    fun <T : BaseUserRegisterModel> createUser(model: T) {
+
+    }
+
+    fun signInWithPhoneNumber(phone: String) {
+
+    }
+
+    fun signInWithEmailAndPassword(email: String, pass: String) {
+
+    }
+
+    fun signInWithToken(token: String): Single<Boolean> {
+
+        return Single.just(true)
+    }
+
+    fun signOut() {
+    }
+
+    fun isInRole(role: String): Boolean {
+        return false
+    }
+}
---
+++ libs/auth/src/main/java/me/mo3in/kroid/auth/models/BaseUserRegisterModel.kt
@@ -0,0 +1,6 @@
+package me.mo3in.kroid.auth.models
+
+open data class BaseUserRegisterModel(
+        val email: String,
+        val password: String
+)
