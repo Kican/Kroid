8cf0c0d20c96c6e375533758903518f7aca4e2e2
mo3in <moein.hente@gmail.com>
2019-05-14 17:31:40 +0430

    some change

--- libs/http/src/main/java/me/mo3in/kroid/http/models/RetroResult.kt
+++ libs/http/src/main/java/me/mo3in/kroid/http/models/RetroResult.kt
@@ -11,72 +11,81 @@ import java.io.IOException
 import java.net.SocketTimeoutException
 
 class RetroResult<T>(val response: Response<T>? = null, throwable: Throwable?) : DataResult<T>(throwable = throwable) {
-    companion object {
-        fun <T> fromData(data: Response<T>): RetroResult<T> {
-            return RetroResult(data, null)
-        }
+	companion object {
+		fun <T> fromData(data: Response<T>): RetroResult<T> {
+			return RetroResult(data, null)
+		}
 
-        fun <T> fromError(error: Throwable): RetroResult<T> {
-            return RetroResult(null, error)
-        }
-    }
+		fun <T> fromError(error: Throwable): RetroResult<T> {
+			return RetroResult(null, error)
+		}
+	}
 
-    override val data: T?
-        get() {
-            if (isSuccessful)
-                return response!!.body()
-            return null
-        }
+	init {
 
-    val statusCode: Int
-        get() {
-            if (response != null)
-                return response.code()
-            return 0
-        }
+	}
 
-    val isSuccessful: Boolean
-        get() {
-            if (response != null && throwable == null && response.isSuccessful)
-                return true
-            return false
-        }
+	override val data: T?
+		get() {
+			if (isSuccessful)
+				return response!!.body()
+			return null
+		}
 
-    var errorType: ResponseErrorType = if (!isSuccessful) {
-        if (response != null) {
-            when (response.code()) {
-                400 -> ResponseErrorType.BadRequestError
-                401, 403 -> ResponseErrorType.UnAuthorizeError
-                else -> ResponseErrorType.ServerError
-            }
-        } else
-            when (throwable) {
-                is SocketTimeoutException -> ResponseErrorType.TimeOutError
-                is IOException -> ResponseErrorType.NetworkError
-                else -> ResponseErrorType.UnknownError
-            }
-    } else
-        ResponseErrorType.Nothing
+	val statusCode: Int
+		get() {
+			if (response != null)
+				return response.code()
+			return 0
+		}
 
-    override val errors: ResultErrors
-        get() {
-            if (response?.errorBody() != null) {
-                val errorBody = response.errorBody()!!.string()
-                if (errorBody.isNotEmpty() && errorType == ResponseErrorType.BadRequestError)
-                    return Gson().fromJson<ResultErrors>(errorBody)
-            }
-            return ResultErrors()
-        }
+	val isSuccessful: Boolean
+		get() {
+			if (response != null && throwable == null && response.isSuccessful)
+				return true
+			return false
+		}
+
+	var errorType: ResponseErrorType = if (!isSuccessful) {
+		if (response != null) {
+			when (response.code()) {
+				400 -> ResponseErrorType.BadRequestError
+				401, 403 -> ResponseErrorType.UnAuthorizeError
+				else -> ResponseErrorType.ServerError
+			}
+		} else
+			when (throwable) {
+				is SocketTimeoutException -> ResponseErrorType.TimeOutError
+				is IOException -> ResponseErrorType.NetworkError
+				else -> ResponseErrorType.UnknownError
+			}
+	} else
+		ResponseErrorType.Nothing
+
+	private var _errors: ResultErrors? = null
+
+	override val errors: ResultErrors
+		get() {
+			if (_errors == null) {
+				if (response?.errorBody() != null) {
+					val errorBody = response.errorBody()!!.string()
+					if (errorBody.isNotEmpty())
+						_errors = Gson().fromJson<ResultErrors>(errorBody)
+				} else
+					_errors = ResultErrors()
+			}
+			return _errors!!
+		}
 
 
-    val message: String = if (response != null) response.message() else if (throwable!!.message != null) throwable.message!! else ""
+	val message: String = if (response != null) response.message() else if (throwable!!.message != null) throwable.message!! else ""
 
-    fun showErrorAsToast(context: Context, showNetworkErrors: Boolean = false) {
-        var message = if (showNetworkErrors) message else ""
+	fun showErrorAsToast(context: Context, showNetworkErrors: Boolean = false) {
+		var message = if (showNetworkErrors) message else ""
 
-        if (errors.size > 0)
-            message = errors.toSeparateRows()
+		if (errors.size > 0)
+			message = errors.toSeparateRows()
 
-        context.toast(message)
-    }
+		context.toast(message)
+	}
 }
--- libs/http/src/test/java/me/mo3in/kroid/http/RunAsyncTest.kt
+++ libs/http/src/test/java/me/mo3in/kroid/http/RunAsyncTest.kt
@@ -5,6 +5,7 @@ import me.mo3in.kroid.commons.models.Result
 import me.mo3in.kroid.commons.services.KServiceConfig
 import me.mo3in.kroid.http.extensions.runAsync
 import me.mo3in.kroid.http.models.ResponseErrorType
+import me.mo3in.kroid.http.models.RetroResult
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Assert
@@ -20,79 +21,80 @@ import java.util.concurrent.atomic.AtomicReference
 
 @RunWith(MockitoJUnitRunner::class)
 class RunAsyncTest {
-    private lateinit var webServer: MockWebServer
+	private lateinit var webServer: MockWebServer
 
-    @Before
-    fun setup() {
-        webServer = MockWebServer()
-        webServer.setDispatcher(RetroTestHttpDispatcher())
-        webServer.start(8880)
-        KServiceConfig.setHttpEndPoint("http://localhost:8880/")
-    }
+	@Before
+	fun setup() {
+		webServer = MockWebServer()
+		webServer.setDispatcher(RetroTestHttpDispatcher())
+		webServer.start(8880)
+		KServiceConfig.setHttpEndPoint("http://localhost:8880/")
+	}
 
-    @After
-    fun tearDown() {
-        webServer.shutdown()
-    }
+	@After
+	fun tearDown() {
+		webServer.shutdown()
+	}
 
-    @Test
-    fun get_async_data_successful() {
-        val latch = CountDownLatch(1)
-        val responseRef = AtomicReference<Array<TestData>>()
+	@Test
+	fun get_async_data_successful() {
+		val latch = CountDownLatch(1)
+		val responseRef = AtomicReference<Array<TestData>>()
 
-        HttpClient.build<TestApiService>().getData().runAsync { retroResult ->
-            responseRef.set(retroResult.data)
-            latch.countDown()
-        }
+		HttpClient.build<TestApiService>().getData().runAsync { retroResult ->
+			responseRef.set(retroResult.data)
+			latch.countDown()
+		}
 
-        latch.await()
-        Assert.assertTrue(responseRef.get().size == 2)
-    }
+		latch.await()
+		Assert.assertTrue(responseRef.get().size == 2)
+	}
 
-    @Test
-    fun get_async_data_forbid() {
-        val latch = CountDownLatch(1)
-        val responseRef = AtomicReference<Boolean>(false)
+	@Test
+	fun get_async_data_forbid() {
+		val latch = CountDownLatch(1)
+		val responseRef = AtomicReference<Boolean>(false)
 
-        HttpClient.build<TestApiService>().forbid().runAsync { retroResult ->
-            if (retroResult.errorType == ResponseErrorType.UnAuthorizeError)
-                responseRef.set(true)
-            latch.countDown()
-        }
+		HttpClient.build<TestApiService>().forbid().runAsync { retroResult ->
+			if (retroResult.errorType == ResponseErrorType.UnAuthorizeError)
+				responseRef.set(true)
+			latch.countDown()
+		}
 
-        latch.await()
-        Assert.assertTrue(responseRef.get())
-    }
+		latch.await()
+		Assert.assertTrue(responseRef.get())
+	}
 
-    @Test
-    fun get_async_data_badrequest() {
-        val latch = CountDownLatch(1)
-        val responseRef = AtomicReference<Boolean>(false)
-        val resultRef = AtomicReference<Result>()
+	@Test
+	fun get_async_data_badrequest() {
+		val latch = CountDownLatch(1)
+		val responseRef = AtomicReference<Boolean>(false)
+		val resultRef = AtomicReference<RetroResult<Array<TestData>>>()
 
-        HttpClient.build<TestApiService>().badRequest().runAsync { retroResult ->
-            System.out.println(retroResult.errors.toJson())
-            if (retroResult.errorType == ResponseErrorType.BadRequestError) {
-                responseRef.set(true)
-                resultRef.set(retroResult)
-            }
-            latch.countDown()
-        }
+		HttpClient.build<TestApiService>().badRequest().runAsync { retroResult ->
+			System.out.println(retroResult.errors.toJson())
+			responseRef.set(true)
+			resultRef.set(retroResult)
 
-        latch.await()
-        Assert.assertTrue(responseRef.get() && resultRef.get().errors!![""]!!.contains("field-is-required"))
-    }
+			latch.countDown()
+		}
 
-    interface TestApiService {
-        @GET("/data")
-        fun getData(): Call<Array<TestData>>
+		latch.await()
+		System.out.println(resultRef.get().errors.toJson())
+		Assert.assertTrue(responseRef.get() && resultRef.get().errorType == ResponseErrorType.BadRequestError && resultRef.get().errors[""]!!.contains("field-is-required"))
 
-        @GET("/error403")
-        fun forbid(): Call<Array<TestData>>
+	}
 
-        @GET("/error400")
-        fun badRequest(): Call<Array<TestData>>
-    }
+	interface TestApiService {
+		@GET("/data")
+		fun getData(): Call<Array<TestData>>
 
-    data class TestData(val title: String, val id: Int)
+		@GET("/error403")
+		fun forbid(): Call<Array<TestData>>
+
+		@GET("/error400")
+		fun badRequest(): Call<Array<TestData>>
+	}
+
+	data class TestData(val title: String, val id: Int)
 }
